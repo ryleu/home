b97e416edf2d8460e021c15026b8d2b6b9d0eb30
ryleu <69326171+ryleu@users.noreply.github.com>
2025-04-16 03:51:13 -0500

    manage ssh config

--- home.nix
+++ home.nix
@@ -126,6 +126,31 @@ in
     thefuck.enable = true;
     firefox.enable = true;
 
+    ssh = {
+      enable = true;
+      addKeysToAgent = "yes";
+      compression = true;
+      matchBlocks = {
+        "github.com" = {
+          hostname = "github.com";
+          user = "git";
+          identityFile = "~/.ssh/github";
+        };
+
+        "ripi" = {
+          hostname = "ripi";
+          user = "ryleu";
+          identityFile = "~/.ssh/ripi";
+        };
+
+        "voron" = {
+          hostname = "192.168.193.62";
+          user = "voron2";
+          identityFile = "~/.ssh/voron";
+        };
+      };
+    };
+
     vim = {
       defaultEditor = true;
       extraConfig = ''
