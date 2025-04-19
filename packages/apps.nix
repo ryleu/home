4f67e2da8d928abe46b15adfd31dd69134d08b51
ryleu <69326171+ryleu@users.noreply.github.com>
2025-04-19 14:25:55 -0500

    add pavucontrol

--- packages/apps.nix
+++ packages/apps.nix
@@ -15,20 +15,21 @@ in
 {
   home = {
     packages = with pkgs; [
-    vlc
-    spotify
-    prismlauncher
-    brave
-    zed-editor
-    vesktop
-    vscode
-    rstudio
-    zotero
-    logseq
-    jetbrains.idea-community-src
-    gabutdm
-    prusa-slicer
-    bambu-studio
+      vlc
+      spotify
+      prismlauncher
+      brave
+      zed-editor
+      vesktop
+      vscode
+      rstudio
+      zotero
+      logseq
+      jetbrains.idea-community-src
+      gabutdm
+      prusa-slicer
+      bambu-studio
+      pavucontrol
     ];
   };
 
