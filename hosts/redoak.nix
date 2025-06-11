fbcc8eb10157d3bfe420a9286a8245968ab8166b
ryleu <69326171+ryleu@users.noreply.github.com>
2025-06-11 14:10:09 -0400

    fix vscode server

--- hosts/redoak.nix
+++ hosts/redoak.nix
@@ -1,4 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    "${fetchTarball {
+      url = "https://github.com/msteen/nixos-vscode-server/tarball/master";
+      sha256 = "sha256:09j4kvsxw1d5dvnhbsgih0icbrxqv90nzf0b589rb5z6gnzwjnqf";
+    }}/modules/vscode-server/home.nix"
+  ];
+
+  services.vscode-server.enable = true;
 }
