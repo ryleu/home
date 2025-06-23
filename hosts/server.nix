e3c5b8d7de2585628216e2a312a07368d8ee0ad3
ryleu <69326171+ryleu@users.noreply.github.com>
2025-06-23 13:33:19 -0500

    update the commit hash for the vscode server fix thing

--- hosts/server.nix
+++ hosts/server.nix
@@ -5,7 +5,7 @@
     "${
       fetchTarball {
         url = "https://github.com/msteen/nixos-vscode-server/tarball/master";
-        sha256 = "sha256:09j4kvsxw1d5dvnhbsgih0icbrxqv90nzf0b589rb5z6gnzwjnqf";
+        sha256 = "sha256:1l77kybmghws3y834b1agb69vs6h4l746ga5xccvz4p1y8wc67h7";
       }
     }/modules/vscode-server/home.nix"
   ];
