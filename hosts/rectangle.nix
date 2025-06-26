c85d40b28810f4a0e56b01871e863a8e84c1e231
ryleu <69326171+ryleu@users.noreply.github.com>
2025-06-26 12:36:51 -0500

    move the monitor to a different port on rectangle

--- hosts/rectangle.nix
+++ hosts/rectangle.nix
@@ -7,7 +7,7 @@
     settings = {
       # See https://wiki.hyprland.org/Configuring/Monitors/
       monitor = [
-        "DP-2,2560x1440@143.91Hz,0x0,1"
+        "DP-3,2560x1440@143.91Hz,0x0,1"
         "HDMI-A-1,1920x1080@74.97Hz,-1080x-480,1,transform,1"
       ];
     };
