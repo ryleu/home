66c686543b124307b98df4bda98c6b95ef19f57c
ryleu <69326171+ryleu@users.noreply.github.com>
2025-07-16 14:03:23 -0500

    perhaps bind workspaces to monitors for barely better

--- hosts/barely-better.nix
+++ hosts/barely-better.nix
@@ -1,8 +1,14 @@
 { ... }:
 
 {
-  wayland.windowManager.hyprland.settings.monitor = [
-    "eDP-1,1920x1080@60.04200Hz,0x0,1"
-    ",preferred,auto,1"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "eDP-1, 1920x1080@60.04200Hz, 0x0, 1"
+      ", preferred, auto, 1"
+    ];
+    workspace = [
+      "1, eDP-1"
+      "10, HDMI-A-1"
+    ];
+  };
 }
