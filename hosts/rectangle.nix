9188c1d8ff72c885cbe1b900e392e35b83e9f5a5
ryleu <69326171+ryleu@users.noreply.github.com>
2025-07-17 22:56:59 -0500

    move monitor back

--- hosts/rectangle.nix
+++ hosts/rectangle.nix
@@ -34,7 +34,7 @@
       # See https://wiki.hyprland.org/Configuring/Monitors/
       monitor = [
         "DP-3,2560x1440@143.91Hz,0x0,1"
-        "HDMI-A-1,1920x1080@74.97Hz,-1080x-480,1,transform,1"
+        "HDMI-A-1,1920x1080@74.97Hz,2560x-480,1,transform,3"
       ];
     };
   };
