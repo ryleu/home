724fe653952b194ce23daf07b727ed7b052b04a1
ryleu <69326171+ryleu@users.noreply.github.com>
2026-01-29 21:19:24 -0500

    change zen settings to remove titlebar

--- conf/zen.nix
+++ conf/zen.nix
@@ -9,73 +9,77 @@
     zen_browser.homeModules.twilight
   ];
 
-  programs.zen-browser = {
-    enable = true;
+  programs.zen-browser =
+    let
+      mkLockedAttrs = builtins.mapAttrs (
+        _: value: {
+          Value = value;
+          Status = "locked";
+        }
+      );
+    in
+    {
+      enable = true;
 
-    nativeMessagingHosts = [ pkgs.firefoxpwa ];
+      nativeMessagingHosts = [ pkgs.firefoxpwa ];
 
-    policies = {
-      AutofillAddressEnabled = false;
-      AutofillCreditCardEnabled = false;
-      DisableAppUpdate = true;
-      DisableFeedbackCommands = true;
-      DisableFirefoxStudies = true;
-      DontCheckDefaultBrowser = true;
-      NoDefaultBookmarks = true;
-      OfferToSaveLogins = false;
-      DisableSetDesktopBackground = true;
+      policies = {
+        AutofillAddressEnabled = false;
+        AutofillCreditCardEnabled = false;
+        DisableAppUpdate = true;
+        DisableFeedbackCommands = true;
+        DisableFirefoxStudies = true;
+        DontCheckDefaultBrowser = true;
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        DisableSetDesktopBackground = true;
 
-      SearchEngines = {
-        Add = [
-          {
-            "Name" = "Unduck";
-            "URLTemplate" = "https://s.dunkirk.sh?q={searchTerms}";
-            "Method" = "GET";
-            "IconURL" = "https://s.dunkirk.sh/favicon.ico";
-            "Alias" = "undk";
-            "Description" = "ddg bangs pwa";
-          }
-        ];
-        Default = "Unduck";
-        PreventInstalls = true;
-      };
+        SearchEngines = {
+          Add = [
+            {
+              "Name" = "Unduck";
+              "URLTemplate" = "https://s.dunkirk.sh?q={searchTerms}";
+              "Method" = "GET";
+              "IconURL" = "https://s.dunkirk.sh/favicon.ico";
+              "Alias" = "undk";
+              "Description" = "ddg bangs pwa";
+            }
+          ];
+          Default = "Unduck";
+          PreventInstalls = true;
+        };
 
-      ExtensionSettings = {
-        "uBlock0@raymondhill.net" = {
-          # ublock
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
+        ExtensionSettings = {
+          "uBlock0@raymondhill.net" = {
+            # ublock
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            # bitwarden
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "admin@2fas.com" = {
+            # 2fas
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/2fas-two-factor-authentication/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "tasksforcanvas@jtchengdev.com" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/tasks-for-canvas/latest.xpi";
+            installation_mode = "force_installed";
+          };
         };
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          # bitwarden
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          installation_mode = "force_installed";
+        Preferences = mkLockedAttrs {
+          "browser.tabs.warnOnClose" = false;
+          "middlemouse.paste" = false;
+          "browser.tabs.inTitlebar" = 0;
         };
-        "admin@2fas.com" = {
-          # 2fas
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/2fas-two-factor-authentication/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "tasksforcanvas@jtchengdev.com" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/tasks-for-canvas/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
-      Preferences = {
-        "browser.tabs.warnOnClose" = {
-          "Value" = false;
-          "Status" = "locked";
-        };
-        "middlemouse.paste" = {
-          "Value" = false;
-          "Status" = "locked";
-        };
-      };
-      Permissions = {
-        Autoplay = {
-          Default = "block-audio-video";
+        Permissions = {
+          Autoplay = {
+            Default = "block-audio-video";
+          };
         };
       };
     };
-  };
 }
